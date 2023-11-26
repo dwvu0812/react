739def75290e68d7721be71490767dd8b34c4e54
Khanh Du Vu <khanhdu1211@gmail.com>
2023-11-27 00:28:10 +0700

    useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { useReducer } from "react";
+type State = {
+  count: number;
+};
+
+type Action = {
+  type: string;
+};
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const initialState: State = {
+  count: 0,
+};
+
 function App() {
-  return <div>App</div>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleIncrement = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  return (
+    <>
+      <h1>Count: {state.count}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+    </>
+  );
 }
 
 export default App;
